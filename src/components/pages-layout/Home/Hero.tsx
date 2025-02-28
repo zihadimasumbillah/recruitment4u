d92d5ae9f06cfc78a9f0dec8dfbfc20dbf2eee8c
Masum Billah Zihadi <zihadimasumbillah@gmail.com>
2025-03-01 02:16:30 +0600

    completed code upload

--- src/components/pages-layout/Home/Hero.tsx
+++ src/components/pages-layout/Home/Hero.tsx
@@ -10,7 +10,7 @@ const Hero = () => {
       "py-12 sm:py-16 md:py-20 lg:py-24 xl:py-32", 
       "bg-primary w-screen",
       "left-[50%] right-[50%] mx-[-50vw] px-0",
-      "min-h-[85vh] md:min-h-[90vh] lg:min-h-[85vh] xl:min-h-[100vh]", 
+      "min-h-[100vh]", 
       "-mt-20" 
     )}>
       <div className="container mx-auto px-4 pt-12 sm:pt-16 md:pt-20 lg:pt-24 w-full"> 
